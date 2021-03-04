dc0ca9c2df962ecc9c41d9ec9697b4ac37a2fe13
Jetniphan Pukkham <jeni8399@gmail.com>
2021-03-04 14:13:58 +0700

    add userlogin

--- ahoy-node-passport/routes/users.js
+++ ahoy-node-passport/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});*/
+
+router.post('/register', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+router.post('/login', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
